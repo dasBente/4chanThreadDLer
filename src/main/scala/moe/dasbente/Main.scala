ff5f515a95b3e703bd908a6f1e50d91183457c61
dasbente <dasbente@gmail.com>
2015-10-19 01:28:10 +0200

    Added basis for a simple CLI

---
+++ src/main/scala/moe/dasbente/Main.scala
@@ -0,0 +1,48 @@
+package moe.dasbente
+ 
+import moe.dasbente.Plunderer
+
+object Main {
+  def main(args: Array[String]) {
+    if (args.length == 0)
+      printHelp() // Print help
+    else if (args.length == 1)
+      Plunderer.plunder(args(0)) // Download to local directory
+    else if (args.length == 2)
+      Plunderer.plunder(args(0), args(1)) // Download to given directory
+    else
+      processArguments(args.toList) // Process argument list
+  }
+
+  def processArguments(args: List[String]) {
+    var directory = "."
+    var threadurl = ""
+    var verbose = false
+
+    args match { // TODO: Needs some fail-safes
+      case "--help" :: xs =>
+        printHelp()
+      case f if (f == "-v" || f == "--verbose") =>
+        verbose = true
+      case f :: v :: xs => {
+        if (f == "-d" || f == "--directory") 
+          directory = v
+        else if (f == "-u" || f == "-url") 
+          threadurl = v
+        else { // Assume that the first argument is the URL and the second is the directory
+          directory = v
+          threadurl = f
+        }
+
+        // Proceed with evaluating the argument list
+        processArguments(xs)
+      }
+    }
+
+    Plunderer.plunder(threadurl, directory, verbose)
+  }
+
+  def printHelp() {
+    println("To be implemented")
+  }
+}
