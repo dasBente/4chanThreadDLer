914d4d9f70bc9bc11423039ba73a749f504b9d8a
dasbente <dasbente@gmail.com>
2015-10-19 01:27:25 +0200

    Rewritten Plunderer for easier use

--- src/main/scala/moe/dasbente/Plunderer.scala
+++ src/main/scala/moe/dasbente/Plunderer.scala
@@ -1,35 +1,36 @@
 package moe.dasbente
+
 import java.io.File
 import java.net.URL
 
 /**
  * @author dasBente
  */
-class Plunderer(val threadurl: String, val savedir: File) {
-  var dledImages: Set[String] = Set()
-  plunder()
-  
-  // Plunder a threads posted images, if not previously downloaded
-  def plunder() {
+
+object Plunderer {
+  def plunder(url: String, dir: String, verbose: Boolean) {
     // Locate all post images anchor tags and extract image URL's
-    val html = io.Source.fromURL(threadurl).mkString.split("<").toList
+    val html = io.Source.fromURL(new URL(url)).mkString.split("<").toList
     val imageTags = html filter (_ startsWith "a class=\"fileThumb\"")
     
     import sys.process._
     for (l <- imageTags) {
       val link = "https:"+ l.split(" ")(2).substring(6).takeWhile(_ != '"') 
       
-      // Check whether link is contained in the Set of downloaded Images, if not, download it
-      if (!(dledImages contains link)) {
-        dledImages += link
-        
-        // Generate filename from link. Prepend absolute file path
-        val filename = savedir.toPath +"\\"+ link.split("/").last
-        
-        println("Downloading from "+ link)
-        // Download image from image URL
-        new URL(link) #> new File(filename) !!
-      }
+      // Generate filename from link. Prepend absolute file path
+      val filename = new File(dir).toPath +
+        (if (System.getProperty("os.name").startsWith("Windows")) "\\" else "/") + 
+          link.split("/").last
+      
+      if (verbose)
+        println("Downloading from "+ link +" to "+ filename)
+      
+      // Download image from image URL
+      new URL(link) #> new File(filename) !!
     }
   }
+
+  def plunder(url: String, dir: String): Unit = plunder(url, dir, false)
+
+  def plunder(url: String): Unit = plunder(url, ".")
 }
