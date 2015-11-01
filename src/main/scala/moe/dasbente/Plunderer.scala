ab2b5b84cd9647e2f9dcb676a0b386409fb616f2
Tobias Möhring <dasbente@gmail.com>
2015-11-02 00:26:58 +0100

    Refactored download in Plunderer
    Downloading in Plunderer is now refactored to a set of overloaded
    methods

--- src/main/scala/moe/dasbente/Plunderer.scala
+++ src/main/scala/moe/dasbente/Plunderer.scala
@@ -14,23 +14,44 @@ object Plunderer {
     val imageTags = html filter (_ startsWith "a class=\"fileThumb\"")
     
     import sys.process._
-    for (l <- imageTags) {
-      val link = "https:"+ l.split(" ")(2).substring(6).takeWhile(_ != '"') 
-      
-      // Generate filename from link. Prepend absolute file path
-      val filename = new File(dir).toPath +
-        (if (System.getProperty("os.name").startsWith("Windows")) "\\" else "/") + 
-          link.split("/").last
-      
-      if (verbose)
-        println("Downloading from "+ link +" to "+ filename)
-      
-      // Download image from image URL
-      new URL(link) #> new File(filename) !!
-    }
+    for (l <- imageTags) 
+      download("https:"+ l.split(" ")(2).substring(6).takeWhile(_ != '"'), dir, verbose) 
   }
-
-  def plunder(url: String, dir: String): Unit = plunder(url, dir, false)
-
-  def plunder(url: String): Unit = plunder(url, ".")
+	
+  def plunder(url: String, dir: String) { plunder(url, dir, false) }
+  def plunder(url: String) { plunder(url, ".") }
+	
+	def download(url: String, dir: String, filename: String, verbose: Boolean) {
+		var path = if (dir startsWith ".") 
+			new File("").getAbsolutePath() + dir.tail
+		else if (dir startsWith "~")
+			System.getProperty("user.home") + dir.tail
+		else dir // TODO: Add support for .. !
+		
+		path = if (System.getProperty("os.name").startsWith("Windows")) 
+			path + "\\" + filename 
+		else 
+			path + "/" + filename
+		
+		if (verbose) 
+			println("Downloading "+ filename +" from "+ url)
+		
+		// Download File from URL to Path
+		import sys.process._
+		new URL(url) #> new File(path) !!
+	}
+	
+	def download(url: String, dir: String, filename: String) {
+		download(url, dir, filename, false)
+	}
+	
+	def download(url: String, dir: String, verbose: Boolean) {
+		val filename = url.split("/").last
+		download(url, dir, filename, verbose)
+	}
+	
+	def download(url: String, dir: String) { download(url, dir, false) }
+	def download(url: String, verbose: Boolean) { download(url, ".", false) }
+	def download(url: String) { download(url, ".", false) }
+	
 }
